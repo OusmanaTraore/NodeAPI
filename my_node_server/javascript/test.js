7b98dccbf0bd2109e3fb1ee21298e4d26a45348b
OusmanaTraore <OusmanaTraore@users.noreply.github.com>
2021-01-04 08:20:32 +0100

    update

--- my_node_server/javascript/test.js
+++ my_node_server/javascript/test.js
@@ -1,55 +1,38 @@
-// setInterval(function () {
-//   let p = "Respect de l'autre!";
-//   let count = p.length;
-//   for (var i = 0; i < p.length.toString() < i++; ) {
-//     //     console.log(p.substring(i));
-//     console.log();
-//   }
-//   //   console.log(p);
-//   //   console.log(p.substring(count));
-// }, 2000);
-
-// console.log(p.length);
-// const p = "Respect de l'autre!";
-// var m = p.length;
-// var success = setInterval();
-// // var l = "";
-// var l = new Promise(function (resolve, reject) {
-//   resolve(success);
-//   //   success;
-// });
-// for (var i = 1; i < 3 < i++; ) {
-//   //   let s = p.slice(0, i);
-//   //   const s = p.slice(0, m);
-//   var s = p.charAt(4);
-//   var l = l + s;
-// }
-// (() => {
-//   for (var j = 0; j < m; j++) {
-//     // p.charAt([j]);
-//     l += p.charAt([j]).then(
-//       setInterval(() => {
-//         console.log(l);
-//       }, 2000)
-//     );
-//     // mot.push(l);
-//   }
-// })();
-
-async function affichage() {
-  const promise = new Promise((resolve, reject) => {
-    setTimeout(() => resolve("ok"), 2000);
+function getRandomColor() {
+  var letters = "0123456789ABCDEF";
+  var color = "#";
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  document.addEventListener("keypress", function (event) {
+    if (event.keyCode == 13) {
+      document.body.style.background = color;
+    } else {
+      document.querySelector(".div1").style.background = color;
+    }
   });
-  let result = await promise;
-  alert(result);
 }
-
-affichage();
-
-// async function test(){
-//     const promise = new Promise((resolve, reject)=>{
-//         setTimeout(()=> resolve('ok'),2000)
-//     });
-//     let result = await promise;
-//     alert(result);
-// }
+function Entrer() {
+  var letters = "0123456789ABCDEF";
+  var color = "#";
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  document.body.style.background = color;
+}
+function controle() {
+  var saisie = document.querySelector("#input").value;
+  //   alert(saisie);
+  alert(`Vous avez saisie ${saisie}`);
+}
+// document.querySelector("#input").hide();
+function afficher() {
+  var saisie = document.querySelector("#input").value;
+  if (saisie.length <= 0) {
+    "#texte2".hide();
+    // document.querySelector("#texte2").css("display", "none");
+    //     "#texte2".hide();
+  } else {
+    document.querySelector("#texte2").value = saisie;
+  }
+}
