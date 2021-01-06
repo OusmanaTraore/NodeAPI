b83a872326fe289322b0f2dccec131d48fc0ee2f
OusmanaTraore <OusmanaTraore@users.noreply.github.com>
2021-01-06 10:33:49 +0100

    formulaires, chekbox

---
+++ Note.md
@@ -0,0 +1,29 @@
+Gestion d'une flotte de véhicules
+
+1 SI
+DSI => Production, system, études
+operations - Development
+
+Infrastructures
+GPS(Localisation et transmission de données)
+
+- Choix de :Topologies, protocole réseau de communication
+  equipements et logistiques( cables, switch, hub ,routeur , firewall ,NAS ,SAN)
+  *Bases de données => data center
+  *Serveurs de traitements (middlewares)
+- Interfaces utilisateurs => system exploitatation
+  Choix de l'architecture (Navigateur)
+
+* informatique formelle
+* informatique informelle (test par les utilisateurs ,hackers)
+* Externes
+
+1,3,5,6,7,9,11,12,13,15,16,17,18,21 switch mac (local (trames)),22 routeur ip (local ,distant, paquets),23,24,25,26,27,28,29,30,31
+34,35,38,39,40
+2c,4a,8b,10c,14b,19b,20d,32b,33c,36c,37a,
+
+Modeles OSI
+Segment couche 4
+=> paquets couche 3
+=> trames couche 2
+=> bits couche 1
--- my_node_server/javascript/test.js
+++ my_node_server/javascript/test.js
@@ -29,10 +29,31 @@ function controle() {
 function afficher() {
   var saisie = document.querySelector("#input").value;
   if (saisie.length <= 0) {
-    "#texte2".hide();
-    // document.querySelector("#texte2").css("display", "none");
-    //     "#texte2".hide();
+    //   // "#texte2".hide();
+    document.getElementsByClassName(".div1").style.display = "none";
+    alert("Veuillez saisir une entrée!");
+    //   //     "#texte2".hide();
   } else {
+    //   document.querySelector("#texte2").css("display", "block");
     document.querySelector("#texte2").value = saisie;
   }
+}
+let butto = document.getElementsById("butto");
+buto.addEventListener("mouseover", () => {
+  // if (getComputedStyle(div3).display != "none") {
+  div3.style.display = "none";
+  // } else {
+  //   div3.style.display = "block";
+  // }
+});
+function onfocusColor() {
+  setTimeout(function () {
+    // alert("loaded");
+    var letters = "0123456789ABCDEF";
+    var color = "#";
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    document.querySelector(".div2").style.background = color;
+  }, 1000);
 }
