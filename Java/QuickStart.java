e08807cf8d0dee53da7fc72c8fddd6e02ee7fdae
OusmanaTraore <OusmanaTraore@users.noreply.github.com>
2021-01-18 08:39:40 +0100

    ComboArticle

--- Java/QuickStart.java
+++ Java/QuickStart.java
@@ -1,5 +1,47 @@
-class Main  {
+class QuickStart  {
     public static void main(String[] args) {
-        System.out.println("Hello, World.");
+       for(var loop =1; loop <=3; loop++){
+           if(loop % 2 == 0 ){
+
+               
+               for (var i = 1 ; i <=20 ; i++){
+                   for(var j =1; j<= 20 ; j++){
+                       if(i % 2 == 0 && j % 2 == 0 || i % 2 == 1 && j % 2 == 1 ){
+                           String x = "X";
+                           System.out.print(x);
+                           
+                        }else if (i % 2 == 1 && j % 2 == 0 || i % 2 == 0 && j % 2 == 1){
+                            String o = "O";
+                            System.out.print(o);
+                            
+                        }
+                        
+                    }
+                    System.out.println();
+                    
+                }
+            }else if(loop % 2 == 1 ){
+                  for (var i = 1 ; i <=20 ; i++){
+                   for(var j =1; j<= 20 ; j++){
+                       if(i % 2 == 0 && j % 2 == 0 || i % 2 == 1 && j % 2 == 1 ){
+                           String x = "G";
+                           System.out.print(x);
+                           
+                        }else if (i % 2 == 1 && j % 2 == 0 || i % 2 == 0 && j % 2 == 1){
+                            String o = "A";
+                            System.out.print(o);
+                            
+                        }
+                        
+                    }
+                    System.out.println();
+                    
+                }
+            }
+
+            
+
+        }
+
     }
 }
